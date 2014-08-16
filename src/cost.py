20acc834159309727c175e59e38e1152dab345bd
ryne beeson <rynebeeson@gmail.com>
2014-08-16 14:52:19 -0500

    updates to .gitignore and re-committing cost.py

---
+++ src/cost.py
@@ -0,0 +1,8 @@
+#  owner: rb
+
+# a function for calculating the mission cost
+def calculate_cost(mission):
+    
+    #  the cost of the mission is added as
+    #+ an attribute to the mission
+    mission.cost = 0.5
