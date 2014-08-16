fe18991c4c791ce509ce8673f7d80fa2c2f520f6
Dreyrden <rjgarner92@gmail.com>
2014-08-16 21:35:10 +0100

    Power Minor Changes
    mission changed to parents

--- src/power.py
+++ src/power.py
@@ -1,6 +1,6 @@
 import math
 
-def calculate_power(mission):
+def calculate_power(parent):
     '''
     mission, tank_area, ZBO,
    '''
@@ -8,18 +8,18 @@ def calculate_power(mission):
     # System Trade-Study Mass/Power Requirements
     system_power = 0.0
     system_mass = 0.0
-    mission.power += system_power
+    parent.power += system_power
 
     orbital_parameter = 3.986E14
     earth_radius = 6378E3
 
-    orbital_period = 2*math.pi()*math.pow(math.pow(mission.ntr.sma, 3)/orbital_parameter,1/2)
-    eclipse_period = (orbital_period*math.asin(earth_radius/mission.ntr.sma))/180
+    orbital_period = 2*math.pi()*math.pow(math.pow(parent.sma, 3)/orbital_parameter,1/2)
+    eclipse_period = (orbital_period*math.asin(earth_radius/parent.sma))/180
 
     # Assuming Peak power tracking regulation scheme Xe = 0.6, Xd = 0.8
     Xe = 0.6
     Xd = 0.8
-    solar_panel_power = (mission.power*eclipse_period/Xe + mission.power*(orbital_period-eclipse_period)/Xd)/Xd
+    solar_panel_power = (parent.power*eclipse_period/Xe + parent.power*(orbital_period-eclipse_period)/Xd)/Xd
 
     panel_efficiency = 0.28
     solar_flux = 1368.00
@@ -35,5 +35,5 @@ def calculate_power(mission):
     solar_panel_area = solar_panel_power/power_end_of_life
     solar_panel_mass = solar_panel_area*M_A
 
-    mission.power.mass += array_mass + system_mass
+    parent.mass += array_mass + system_mass
 
